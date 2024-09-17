5ea8949fb3385ba5bef27d39da9f6eb4dec145a6
bkbehera <behera.binaya@gmail.com>
2024-09-17 10:55:57 +0530

    Intial bootstrap

---
+++ 01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,17 @@
+// String
+const firstName = 'Sara';
+
+// Number
+const age = 30;
+
+const temp = 98.9;
+
+// boolean
+const hasKids = true;
+
+// Null
+const aptNumbner = null;
+
+const output = aptNumbner;
+
+console.log(output, typeof output);
